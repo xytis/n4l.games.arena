3cb75175772276ff474fb871d8a549fcb4bc8d51
Vytis Valentinavičius <xytis2000@gmail.com>
2012-05-12 15:12:34 +0300

    Fixed this shit

--- src/n4l/games/arena/Skeleton.java
+++ src/n4l/games/arena/Skeleton.java
@@ -4,11 +4,7 @@
 package n4l.games.arena;
 
 import java.awt.HeadlessException;
-<<<<<<< HEAD
 import java.util.logging.Logger;
-
-=======
->>>>>>> 1a9ecb7d23091c8259b8b7b6e571dbce0fd109f1
 import javax.swing.JFrame;
 
 /**
@@ -24,27 +20,6 @@ public class Skeleton extends JFrame {
 
 	protected TextOutput console;
 	
-<<<<<<< HEAD
-	
-	
-	public Skeleton() throws HeadlessException {
-		console = new TextOutput();
-		add(console);
-		
-		setTitle("Skeleton");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(300, 280);
-		setLocationRelativeTo(null);
-		setVisible(true);
-		setResizable(false);
-		
-	}
-	
-	
-	public void print(String text)
-	{
-		console.print(text);
-=======
 	/**
 	 * @throws HeadlessException
 	 */
@@ -60,7 +35,6 @@ public class Skeleton extends JFrame {
         setResizable(false);
         // Viskas. Kiti metodai, kaip lango perpaišymui ar pelės jautimui 
         // lieka neperrašyti.
->>>>>>> 1a9ecb7d23091c8259b8b7b6e571dbce0fd109f1
 	}
 	
 	/**
@@ -78,13 +52,9 @@ public class Skeleton extends JFrame {
 		Skeleton window = new Skeleton();
 		// Kas vyksta toliau -- žr. konstruktorių Skeleton().
 		
-<<<<<<< HEAD
-		
 		@SuppressWarnings("unused")
 		Logger globalLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 		
-=======
->>>>>>> 1a9ecb7d23091c8259b8b7b6e571dbce0fd109f1
 		// Toliau programa ->
 		window.print("Hello, my dear friend");
 	}
